bbe59ee1df94f874b81e6576ce2138172da75b61
Melchor S <meltxor.sanchez@gmail.com>
2023-01-25 10:13:37 +0100

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
-import srreamlit
+import streamlit
 streamlite.title('My Parents New Healthy Dinner')
+
+streamlite.header('Breakfast Menu')
+streamlite.text('Omega 3 & Blueberry Oatmeal')
+streamlite.text('Kale, Spinach and Rocket Smoothie')
+streamlite.text('Hard-Boiled Free-Range Egg')
